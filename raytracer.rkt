d4300f3a1328ffbd1eca354952045eb812c710e1
William Byrd <webyrd@gmail.com>
2020-04-17 20:10:32 -0500

    added sq function

--- raytracer.rkt
+++ raytracer.rkt
@@ -1,8 +1,10 @@
-#lang racket
-
 ;; Simple ray-tracer, adapted from section 9.8
 ;; of Paul Graham's 'ANSI Common Lisp',
 ;; 1996, Prentice-Hall Inc.
 ;;
 ;; Adapted by Clinton Campbell and Will Byrd
+
+;; Math utilities from Fig 9.2
+
+(define sq (lambda (x) (* x x)))
 
