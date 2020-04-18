44f9f6b3b975dcfe76aedbf867f15a6f32e89824
William Byrd <webyrd@gmail.com>
2020-04-17 19:41:44 -0500

    initial import

---
+++ raytracer.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+;; Simple ray-tracer, adapted from section 9.8
+;; of Paul Graham's 'ANSI Common Lisp',
+;; 1996, Prentice-Hall Inc.
+;;
+;; Adapted by Clinton Campbell and Will Byrd
+
