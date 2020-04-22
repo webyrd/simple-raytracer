bdc3de05a6750a38d6fdf16c120705975893cb0c
William Byrd <webyrd@gmail.com>
2020-04-22 14:03:55 -0500

    made it a racket file

--- raytracer.rkt
+++ raytracer.rkt
@@ -1,3 +1,8 @@
+#lang racket
+
+(provide
+  ray-test)
+
 ;; Simple ray-tracer, adapted from section 9.8
 ;; of Paul Graham's 'ANSI Common Lisp',
 ;; 1996, Prentice-Hall Inc.
@@ -5,6 +10,7 @@
 ;; Adapted by Clinton Campbell and Will Byrd
 
 ;; Math utilities from Fig 9.2
+
 
 (define sq (lambda (x) (* x x)))
 
